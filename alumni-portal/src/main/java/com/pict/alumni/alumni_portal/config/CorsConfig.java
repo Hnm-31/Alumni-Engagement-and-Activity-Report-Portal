0da75fa016c33c64abe7113b59da41c87321a60e
HNM <hemantmarathe3108@gmail.com>
2026-04-07 19:10:24 +0530

    feat: enable CORS for frontend requests

--- alumni-portal/src/main/java/com/pict/alumni/alumni_portal/config/CorsConfig.java
+++ alumni-portal/src/main/java/com/pict/alumni/alumni_portal/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Allow all origins (you can specify specific origins if needed)
+                .allowedOriginPatterns("*") // Allow all origins (you can specify specific origins if needed)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
